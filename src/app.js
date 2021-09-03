018328bbaf23bae77880382a201b7e6d4656dc30
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-09-03 18:46:44 +0500

    added express static web view

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
+const path = require("path");
 const express = require("express");
 const app = express();
+
+const staticPath = path.join(__dirname, "../public");
+
+app.use(express.static(staticPath));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
