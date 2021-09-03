c045b2c7fa9ccdfc70db8e80e2955cece7404f8c
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-09-03 17:08:45 +0500

    added json response example

--- nav.js
+++ nav.js
@@ -14,6 +14,21 @@ app.get("/contact", (req, res) => {
   res.status(200).send("This is a contact page.");
 });
 
+app.get("/tamp", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "ovais",
+      company: "abc",
+    },
+    {
+      id: 1,
+      name: "raza",
+      company: "xyz",
+    },
+  ]);
+});
+
 app.listen(port, () => {
   console.log("listen....");
 });
