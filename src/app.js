4faf17a09192b7c558b8e7462af818d38242310a
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-09-06 21:12:14 +0500

    added partials folder example

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require("hbs");
 
 // built in middleware
 const staticPath = path.join(__dirname, "../public");
-const changePath = path.join(__dirname, "./templates");
+const changePath = path.join(__dirname, "./templates/views");
+const partialPath = path.join(__dirname, "./templates/partials");
 // app.use(express.static(staticPath));
 
 // to set the view engine
@@ -12,6 +14,9 @@ app.set("view engine", "hbs");
 
 // added change views directory name example
 app.set("views", changePath);
+
+// register partials path
+hbs.registerPartials(partialPath);
 
 // added dynamic data example
 app.get("/", (req, res) => {
