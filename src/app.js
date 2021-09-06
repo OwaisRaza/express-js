3d67766b39b10b8f1606c9f1d401e401aa875d4a
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-09-06 14:02:20 +0500

    changed views directory name and implemented logic

--- src/app.js
+++ src/app.js
@@ -4,16 +4,24 @@ const app = express();
 
 // built in middleware
 const staticPath = path.join(__dirname, "../public");
+const changePath = path.join(__dirname, "./templates");
 // app.use(express.static(staticPath));
 
 // to set the view engine
 app.set("view engine", "hbs");
+
+// added change views directory name example
+app.set("views", changePath);
 
 // added dynamic data example
 app.get("/", (req, res) => {
   res.render("index", {
     name: "Express",
   });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.get("/", (req, res) => {
