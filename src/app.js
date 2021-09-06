c17a69bbd527705466d872accbde3681fe9f321d
Owais Raza khanzada <owaisraza1989@gmail.com>
2021-09-06 13:16:03 +0500

    added dynamic content in express js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,19 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+// built in middleware
 const staticPath = path.join(__dirname, "../public");
+// app.use(express.static(staticPath));
 
-app.use(express.static(staticPath));
+// to set the view engine
+app.set("view engine", "hbs");
+
+// added dynamic data example
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "Express",
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
